c04ca9bcc3991eb78489840605bcbc9dcf1381f8
Douglas <wdmdavison@icloud.com>
2022-10-01 22:24:04 -0700

    upload first portion of file storage class

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ base_model module for HBNB console project"""
 from datetime import datetime, time
+import models
 import uuid
 time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -33,6 +34,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ Override the string to print
@@ -44,7 +46,7 @@ class BaseModel:
         """ Updates the public instance attribute updated_at
         with the current date and time"""
         self.updated_at = datetime.now()
-        # add storage system and save the file
+        models.storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all of the
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""this module contains the class File Storage"""
+
+import json
+import os.path
+from models.base_model import BaseModel
+
+class FileStorage:
+    """class that serializes instances to a JSON file, and
+    deserializes a JSON file to instances"""
+    
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary storing all objects by <class name>.id
+        i.e., a BaseModel object with id=8675309 would be stored with the
+        key BaseModel.8675309"""
+        return self.__objects
+    
+    
+    def new(self, obj):
+        """sets in the dictionary __objects, the obj and gives it the key
+        in the format of <obj class name>.id"""
+        if obj:
+            self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+
+    def save(self):
+        """serializes the dictionary __objects to the JSON file with the
+        path of __file_path, which allows us to store the objects"""
+        dicttodump = {}
+        for quay in self.__objects:
+            dicttodump[quay] = self.__objects[quay].to_dict()
+        with open(self.__file_path, 'w') as a_file:
+            json.dump(dicttodump, a_file)
+
+
+    def reload(self):
+        """deserializes the JSON file with the path of '__file_path' to
+        __objects, if it exists; otherwise, this method does nothing and
+        doesn't even raise an exception because we're extremely reasonable
+        people who write extremely reasonable code"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file_that_exists:
+                deserialized_dict = json.load(file_that_exists)
+                # THIS IS WHERE I LEFT OFF
+                # THIS FUNCTION IS NOT DONE
