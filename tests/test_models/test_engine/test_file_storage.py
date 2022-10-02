9cc6320fe3486af0372bfa8843eca5abd5c9ec7a
Douglas <wdmdavison@icloud.com>
2022-10-02 13:41:04 -0700

    add a few filestorage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Module with unit tests for the BaseModel Class"""
+from models.engine.file_storage import file_storage
+import unittest
+
+
+class Test_FileStorage_Documentation(unittest.TestCase):
+    """Contains tests for the doc strings in FileStorage"""
+
+    def test_module_docs(self):
+        """Checks for documentation in file_storage module"""
+        self.assertGreaterEqual(len(file_storage.__doc__), 1)
+
+    def test_class_docs(self):
+        """Checks for documentation of FileStorage"""
+        self.assertGreaterEqual(len(file_storage.FileStorage.__doc__), 1)
+
+if __name__ == '__main__':
+    unittest.main()
