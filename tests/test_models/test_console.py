2ba697f0108d282053fddc5c3e682c0a2bffe2f4
Vernon-444 <mt.vernon44@gmail.com>
2022-10-02 22:36:53 -0700

    added one test, going to bed

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -10,3 +10,18 @@ Unittest classes:
     TestHBNBCommand_destroy
     TestHBNBCommand_update
 """
+
+import os
+import sys
+import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    """Unittests for testing prompting of the HBNB command interpreter."""
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    
