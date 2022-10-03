775ec026d7f6a09e4dde4a89ea2e4afe5c4e9a75
Vernon-444 <mt.vernon44@gmail.com>
2022-10-03 11:10:21 -0700

    added user model

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class User that inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class that inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
